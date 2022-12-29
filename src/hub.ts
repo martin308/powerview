0f24c21de4c88630987aab0407bcc0bacde07208
Martin Holman <me@martinholman.co.nz>
2022-12-29 15:12:34 -0800

    Disable any lint rule here

--- src/hub.ts
+++ src/hub.ts
@@ -3,6 +3,7 @@ import { URL } from 'url';
 import EventSource from './eventsource';
 
 interface Logger {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   error(message: string, ...parameters: any[]): void;
 }
 
